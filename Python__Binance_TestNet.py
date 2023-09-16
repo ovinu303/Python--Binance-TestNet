3f6b63680b89f58907e8fae56976f6c037f2920b
ovinu <ovinu21@gmail.com>
2023-09-16 17:38:05 +0530

    Testing

--- Python__Binance_TestNet.py
+++ Python__Binance_TestNet.py
@@ -3,7 +3,7 @@ from binance.client import Client
 # Initialize the Binance client
 client = Client('YOUR_API_KEY', 'YOUR_SECRET_KEY')
 
-# Switch to the testnet
+#Switch to the testnet
 client.API_URL = 'https://testnet.binance.vision/api'
 
 # Get account information
