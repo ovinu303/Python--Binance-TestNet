f21f515ed1f3a1fe7bd102a98e9cbec7a1cb71c1
ovinu <ovinu21@gmail.com>
2023-09-16 17:34:56 +0530

    Add project files.

---
+++ Python__Binance_TestNet.py
@@ -0,0 +1,14 @@
+from binance.client import Client
+
+# Initialize the Binance client
+client = Client('YOUR_API_KEY', 'YOUR_SECRET_KEY')
+
+# Switch to the testnet
+client.API_URL = 'https://testnet.binance.vision/api'
+
+# Get account information
+info = client.get_account()
+
+# Print all balances
+for asset in info['balances']:
+    print(f"{asset['asset']} Balance: {asset['free']}")
